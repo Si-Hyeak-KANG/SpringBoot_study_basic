ffc3bd61c434c1152255bc65de6f6fe80e355bee
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-02-09 15:34:45 +0900

    자바코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서는 동시성 문제로 공유되는 변수일 때는 컨크로트해쉬에이블? 사용
